044efcc87e990c6f65de68fa93b37f52704e7297
Brandon Hayduk <bmhayduk@cs.ualberta.ca>
2014-03-03 19:06:41 -0700

    Framework for models.py

--- plkr/main/models.py
+++ plkr/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+Class Author(models.Model):
+
+
+
+Class Post(models.Model):
+    '''
+    id = models.CharField(max_length=128, primary_key=True, unique=True)
+    title = models.CharField(max_length=128)
+    source = model.URLField()
+    contentType = model.CharField(max_length = 20)
+    origin = model.URLField()
+    description = model.CharField(max_length = 140)
+    content = model.CharField(max_length = 1200)
+    categories = 
+    '''
+
+Class Comment(model.Model):
+
+
+
+Class FriendsList(model.Model):
+
+
