e76e005179a5bf740b7f1b1f7fc3502325260173
Erick Eduardo Ochoa Lopez <eochoa@ualberta.ca>
2014-03-03 17:49:03 -0700

    Created the conf url file

---
+++ plkr/main/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from main import views
+
+urlpatterns = patterns('',
+		url(r'^$', views.index, name="index" ),
+		#authors path
+		url(r'^author/(?P<user_id>\w+)/$', views.get_author, name="get_author"),
+		url(r'^author/post/(?P<post_id>\w+)/$', views.get_author_post,
+			name="authors_posts"),
+		#friends path
+		url(r'^friends/(?P<user_id>\w+)/$', views.get_friends,
+			name="get_friends"),
+		url(r'^friends/(?P<user1_id>\w+)/(?P<user2_id>\w+)/$',
+			views.are_friends, name="are_friends"),
+		#post path
+		url(r'^post/(?P<post_id>\w+)/$', views.posts, name="posts"),
+		)
