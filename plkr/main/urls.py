95356f44f5425a1d5d7f3bbfbe01b003205d976e
dluces <diego.lucesrosado@gmail.com>
2014-03-09 20:37:20 -0600

    Fixed the views.py file and added a route for the timeline

--- plkr/main/urls.py
+++ plkr/main/urls.py
@@ -2,29 +2,32 @@ from django.conf.urls import patterns, include, url
 from main import views
 
 urlpatterns = patterns('',
-		url(r'^$', 'main.views.index', name="index"),
+		url(r'^$', 'main.views.index', name='index'),
 
 		# API URLs
 		
 		# Author - Specific one
-		url(r'^author/(?P<user_id>\w+)$', 'main.views.author', name="get_author"),
-		#url(r'^author/post/(?P<post_id>\w+)$', views.get_author_post, name="authors_posts"),
+		url(r'^author/(?P<user_id>\w+)$', 'main.views.author', name='get_author'),
+		#url(r'^author/post/(?P<post_id>\w+)$', views.get_author_post, name='authors_posts'),
 		
 		# Friends - Of an author
-		url(r'^friends/(?P<user1_id>\w+)$', 'main.views.friends', name="get_friends"),
+		url(r'^friends/(?P<user1_id>\w+)$', 'main.views.friends', name='get_friends'),
 		# Friends - Check friendship
-		url(r'^friends/(?P<user1_id>\w+)/(?P<user2_id>\w+)$', 'main.views.friends', name="are_friends"),
+		url(r'^friends/(?P<user1_id>\w+)/(?P<user2_id>\w+)$', 'main.views.friends', name='are_friends'),
 		# Friends - Send request
-		url(r'^friendrequest$', 'main.views.friendrequest', name="friendrequest"),
+		url(r'^friendrequest$', 'main.views.friendrequest', name='friendrequest'),
 		
 		# Posts - Specific one
-		url(r'^post/(?P<post_id>\w+)$', 'main.views.posts', name="posts"),
+		url(r'^post/(?P<post_id>\w+)$', 'main.views.posts', name='posts'),
 		
 		# Session - Login
 		url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'main/login.html'}),
 		# Session - Logout
 		url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'index'}),
 
+		# Timeline
+		url(r'^site/$', 'main.views.timeline', name='timeline'),
+
 		# 404 Error
-		url(r'^(admin/)^$', 'main.views.notfound', name="notfound"),
+		url(r'^.*$', 'main.views.notfound', name='notfound'),
 		)
