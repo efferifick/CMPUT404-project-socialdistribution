f68b14c7dc9a34a7fd7a0009c598765a740a0166
Erick Eduardo Ochoa Lopez <eochoa@ualberta.ca>
2014-04-01 13:53:21 -0600

    output files were wrong

--- tests/curlcmd.sh
+++ tests/curlcmd.sh
@@ -6,20 +6,21 @@
 
 F_REQUEST_FILE="f_request.json"
 F_QUERY_FILE="f_query.json" 
-AUTHOR_FILE="author.json" #TODO Can we update just from a json file?
+AUTHOR_FILE="author.json" 
 POST_QUERY_FILE="post_query.json" 
 POST_UPDATE_FILE="post_update.json"
 
 USERID1="108ded43-8520-4035-a262-547454d32022"
 USERID2="108ded43-8520-4035-a262-547454d32023"
-POSTID="108ded43-8520-4035-a262-547454d32038"
+POSTID="108ded43-8520-4035-a262-547454d32026"
 
 FRIEND_REQUEST_URL="http://127.0.0.1:8000/friendrequest"
 FRIEND_QUERY_URL="http://127.0.0.1:8000/friends/$USERID1"
 F2F_QUERY_URL="http://127.0.0.1:8000/friends/$USERID1/$USERID2"
 AUTHOR_URL="http://127.0.0.1:8000/author/$USERID1"
 POST_UPDATE_URL="http://127.0.0.1:8000/post/$POSTID"
-POST_QUERY_URL=$POST_UPDATE_URL
+POST_QUERY_URL="http://127.0.0.1:8000/post/$POSTID"
+
 
 F_REQUEST_OUT_FILE="f_req_out.txt"
 FRIEND_QUERY_OUT_FILE="f_query_out.txt"
@@ -32,6 +33,7 @@ POST_UPDATE_OUT_FILE="post_update_out.txt"
 
 
 #Friend request
+echo "Friend request test."
 curl \
 	-s \
 	-H "Content-Type: application/json" \
@@ -41,6 +43,7 @@ curl \
 	$FRIEND_REQUEST_URL \
 	2>&1
 
+echo "Friend query via POST"
 # Friend querying via POSTs to http://127.0.0.1:8000/friends/<userid>
 curl \
 	-s \
@@ -51,6 +54,7 @@ curl \
 	$FRIEND_QUERY_URL \
 	2>&1
 
+echo "Friend query via URL"
 # Friend 2 Friend querying via GETs to http://127.0.0.1:8000.friends/userid1/userid2
 curl \
 	-s \
@@ -60,6 +64,7 @@ curl \
 	$F2F_QUERY_URL \
 	2>&1
 
+echo "Get author profile"
 # Get author profile
  curl \
 	-s \
@@ -69,6 +74,7 @@ curl \
 	$AUTHOR_URL \
 	2>&1
 
+echo "Update author profile via PUT"
 # Update author profile with put
 curl \
 	-s \
@@ -79,25 +85,26 @@ curl \
 	$AUTHOR_URL \
 	2>&1
 
+echo "get author's post via GET"
 # Get posts
 curl \
 	-s \
-	-H "Content-Type: application/json"\
        	-X GET \
-	-o $POST_OUT_FILE \
-	$POST_QUERY_URL \
-	2>&1
+	-o $POST_QUERY_OUT_FILE \
+	$POST_QUERY_URL
 
+echo "Get author's post via POST"
 # Post (which should get) the post
 curl \
 	-s \
 	-H "Content-Type: application/json"\
        	-X POST \
 	--data "@$POST_QUERY_FILE" \
-	-o $POST_2_OUT_FILE \
+	-o $POST_QUERY_OUT_FILE\
 	$POST_QUERY_URL \
 	2>&1
 
+echo "Update or insert post via PUT"
 # Put (should insert or update) the post
 curl \
 	-s \
