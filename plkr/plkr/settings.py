aaacd8e3b2484d229aee58319f8ce8013a31711f
bmhayduk <bmhayduk@ualberta.ca>
2014-04-04 20:47:27 -0600

    Updated user icons

--- plkr/plkr/settings.py
+++ plkr/plkr/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = (
     'markdown_deux',
     'main',
     'dajaxice',
+    'simplegravatar',
 )
+
+SIMPLEGRAVATAR_DEFAULT = 'http://simpleicon.com/wp-content/uploads/user1.png'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
